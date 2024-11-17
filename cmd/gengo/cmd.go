55cbd8be34032d07b75762f5bc9be902914c3470
naonao2323 <yanagidanaoki9900@gmail.com>
2024-11-17 11:17:48 +0900

    feat: mascot

--- cmd/gengo/cmd.go
+++ cmd/gengo/cmd.go
@@ -9,10 +9,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func printMascot() {
+	fmt.Println("\033[34m GGG   EEEEE  N   N  GGG   OOO")
+	fmt.Println("G   G  E      NN  N G   G O   O")
+	fmt.Println("G      EEEE   N N N G     O   O")
+	fmt.Println("G  GG  E      N  NN G  GG O   O")
+	fmt.Println(" GGG   EEEEE  N   N  GGG   OOO\033[0m")
+}
+
+func runParent(cmd *cobra.Command, args []string) {
+	printMascot()
+}
+
 func main() {
 	gengoCmd := &cobra.Command{
-		Use:   "gengo",
-		Short: "Gengo CLI tool",
+		Use:              "gengo",
+		Short:            "Gengo CLI tool",
+		PersistentPreRun: runParent,
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Please use a subcommand like 'gengo gen' or 'gengo dao'.")
 		},
